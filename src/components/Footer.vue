83c6eea19f9cce719050cf04fb6530cf76127d46
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-07-20 16:21:37 +0300

    Add z-index to footer to be on top

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full border-t-2 px-8 py-6 bg-[#f3f4f6] fixed bottom-0 lg:flex justify-between items-center">
+    <div class="w-full border-t-2 px-8 py-6 bg-[#f3f4f6] fixed bottom-0 z-50 lg:flex justify-between items-center">
         <p class="mb-2 lg:mb-0">© Copyright 2024</p>
     </div>
 </template>
